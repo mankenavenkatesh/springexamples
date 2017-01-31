38431bcb5e2038c99bb704c1031b9e7d421f0c96
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2017-01-31 10:47:57 +0530

    adding threadpoolexecutor test

---
+++ springbatch/src/main/java/com/test/spring/springbatch/TestThreadPoolExecutor.java
@@ -0,0 +1,13 @@
+package com.test.spring.springbatch;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+public class TestThreadPoolExecutor {
+
+	public static void main(String[] args) {
+		ThreadPoolExecutor x=new ThreadPoolTaskExecutor();
+		
+	}
+}
